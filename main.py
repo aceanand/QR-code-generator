64b025b1130832257bf7383ac0043a8941688bcb
aceanand <145894327+aceanand@users.noreply.github.com>
2023-12-30 02:25:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import qrcode as qr
+img= qr.make("https://portfolio-ashwini-anands-projects.vercel.app/")
+img.save("portfolio.png")
+img= qr.make("https://github.com/aceanand")
+
+img.save("git.png")
+img= qr.make("https://www.linkedin.com/in/ashwini-anand920/")
+
+img.save("linkdin.png")
+
+img = qr.make("https://amazon-clone-jnyo6830n-ashwini-anands-projects.vercel.app/")
+img.save("Amazon Clone.png")
