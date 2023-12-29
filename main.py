499d0752b4783e593c45b2c48879b3a3823f9fb6
aceanand <145894327+aceanand@users.noreply.github.com>
2023-12-30 02:27:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,5 @@ img.save("linkdin.png")
 
 img = qr.make("https://amazon-clone-jnyo6830n-ashwini-anands-projects.vercel.app/")
 img.save("Amazon Clone.png")
+img = qr.make("https://www.coursera.org/account/accomplishments/certificate/5MWHHY5R9CDN")
+img.save(react.png)
